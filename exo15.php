3c95db10a0547453a8a0323908fcf26cd85fb9d5
Rayan.Raihani <rayanraihani9@gmail.com>
2023-06-13 16:50:51 +0200

    toostring a ajouter

--- exo15.php
+++ exo15.php
@@ -1,28 +1,49 @@
 <?php 
 class Personne{
-    private $_nom;
-    private $_prenom;
-    private $_ddn;
+    private string $_nom;
+    private string $_prenom;
+    private DateTime $_ddn;
 
-public function __construct($nom, $prenom, $ddn){
-    $this->_nom = $nom;
-    $this->_prenom = $prenom;
-    $this->_ddn = new DateTime($ddn);
-}
+    public function __construct(string $nom, string $prenom, string $ddn){
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_ddn = new DateTime($ddn);
+    }
 
-public function getNom() {
-    return $this->_nom;
-}
+    public function getNom() {
+        return $this->_nom;
+    }
+    
+    public function setNom(string $nom) {
+        $this->_nom = $nom;
+    }
 
-public function getPrenom() {
-    return $this->_prenom;
-}
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+    
+    public function setPrenom(string $prenom) {
+        $this->_prenom = $prenom;
+    }
+    
+    public function getDdn() {
+        return $this->_ddn;
+    }
+    
+    public function setDdn(DateTime $ddn) {
+        $this->_ddn = $ddn;
+    }
 
-public function getAge() {
-    $now=new DateTime ();
-    $this->_ddn=$this->_ddn->diff($now);
-    return $this->_ddn->y ;
-}
+    public function getAge() {
+        $now=new DateTime ();
+        $this->_ddn=$this->_ddn->diff($now);
+        return $this->_ddn->y ;
+    }
+
+    public function __toString()
+    {
+        return $p1->getNom()." ". $p1->getPrenom()." a ".$p1->getAge()." ans <br>";;
+    }
 }
 
 $p1 =new Personne("DUPONT", "Michel", "1980-02-19") ;
